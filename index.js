9b76b424097eb3867ab11a65ff2b9e646087dee8
Glyne Gittens <jgit121@gmail.com>
2018-07-05 17:48:56 -0400

    Added database connection code

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var fs = require('fs');
 
 app = express();
 
+/* routes */
+
 
 // Middleware
 app.use(bodyParser.json());
@@ -22,6 +24,30 @@ app.get('/', (req, res) => {
 
 // app.use('/', index);
 
-app.listen(3000, () => {console.log("app running")});
 
+// To run app without database access uncomment the next line 
+// app.listen(3000, () => {console.log("app running")});
+
+
+// Comment out MongoClient block to run without DB client
+// start of MongoClient Block
+// Doesn't start app without successful connection to DB
+MongoClient.connect("mongodb://localhost:27017/livepad", function(err, database){
+	if(err){
+		console.log(err);
+	}
+	else {
+		// coneect database instance to the request object
+		app.use( (req, res, next) => {
+			req.db = database;
+			console.log(req.db);
+			next()
+		})
+		express.request.db = database;
+		console.log("DB CONNECTED!");
+		app.listen(3000, () => { console.log(" App Listening on Port 3000 ") }) 
+	}
+})
+
+// end of MongoClient Block
 module.exports = app;
