b92c1a4998d726c9d9b362dcaedaa78d821eefaa
kre64 <kre9404@gmail.com>
2018-07-12 18:11:46 -0400

    ahh

--- public/js/index.js
+++ public/js/index.js
@@ -10,27 +10,70 @@ var context = new AudioContext();
 var song = context.createMediaStreamDestination();
 var mediaRecorder = new MediaRecorder(song.stream);
 
+//get Tempo(bpm) from input field and turn it into ms.
+function tempoToMs(){
+  var tempo = document.getElementById("tempo").value;
+  var bpm = tempo;
+  var ms = 0;
+  if(bpm <= 0){
+    alert("That's quite a low tempo.");
+  }
+  else{
+    ms = 60000/bpm;
+    //console.log(tempo, ms)
+  }
+  return ms;
+}
 
-//bool used in record() . I have no clue how Js booleans works but this works for me.
-var bool = 0;
+var start_time;
+var total_min = 0;
+var total_sec = 0;
+var end_time;
+
+//bool used in record()
+var is_recording = false;
 
 // records audio, check if record button element is on or off
 function record(){
   r = document.getElementById("rec");
-  if(bool == 0){
-    r.style.color = "purple"
-    bool = 1;
-    mediaRecorder.start(500);
+  if(is_recording == false){
+    r.style.color = "purple";
+    r.innerHTML = "RECORDING";
+    is_recording = true;
+    mediaRecorder.start(tempoToMs());
     play();
+    startT();
+    //call a function that only records through row once.
   }
-  else if(bool == 1){
-    r.style.color = "orange"
-    bool = 0;
+  else if(is_recording == true){
+    endT();
+    r.style.color = "orange";
+    r.innerHTML = "CONTINUE RECORDING";
+    is_recording = false;
     clearInterval(clock);
     mediaRecorder.stop();
     mediaRecorder.release();
     mediaRecorder.reset();
   }
+}
+
+function startT(){
+  start_time = new Date();
+}
+
+function endT(){
+  end_time = new Date();
+  
+  var elapsed_sec = end_time.getSeconds() - start_time.getSeconds();
+  var elapsed_min = end_time.getMinutes() - start_time.getMinutes();
+  
+  console.log(elapsed_sec, elapsed_min);
+  
+  total_min = total_min + elapsed_min;
+  total_sec = total_sec + elapsed_sec;
+  
+  var user_time = document.getElementById("userTime");
+  user_time.innerHTML = total_min + " minute(s), " + total_sec + " seconds.";
 }
 
 mediaRecorder.ondataavailable = function(e){
@@ -138,47 +181,116 @@ function playAudio(element){
 
 //turns buttons on/off
 function set(element, color){
-  if(element.style.background == color){
-    element.style.background = def_color;
+  if(element.style.backgroundColor == color){
+    element.style.backgroundColor = def_color;
   }
   else{
-    element.style.background = color;
+    element.style.backgroundColor = color;
     //remove the following line eventually
     playAudio(element);
   }
 }
 
+function stop(){
+  s = document.getElementById("stop");
+  s.addEventListener("click", function(){
+    clearInterval(clock);
+    is_playing = false;
+    //console.log("uhde");
+  });
+}
+
+function boom(element){
+  var elem = element;
+  //save old button values, set new values for visual effect
+  var original_size = elem.style.borderWidth;
+  elem.style.borderWidth = "4px";
+  
+  var original_color = elem.style.backgroundColor;
+  
+  
+  //make this more programmatic eventually, or scrap it?
+  if(original_color == "mediumseagreen"){
+    elem.style.backgroundColor = "#66cc93";
+    elem.style.borderColor = original_color;
+  }
+  if(original_color == "dodgerblue"){
+    elem.style.backgroundColor = "#339aff";
+    elem.style.borderColor = original_color;
+  }
+  if(original_color == "orange"){
+    elem.style.backgroundColor = "#ffb733";
+    elem.style.borderColor = original_color;
+  }
+  if(original_color == "tomato"){
+    elem.style.backgroundColor = "#ff6347";
+    elem.style.borderColor = original_color;
+  }
+  
+  
+  setTimeout(function(){
+    elem.style.borderWidth = "3px";
+  }, tempoToMs()/4 );
+  
+  setTimeout(function(){
+    elem.style.border = original_size;
+    elem.style.backgroundColor = original_color;
+    elem.style.borderColor = "rgba(180,180,180,180)";
+  }, tempoToMs() );
+}
+
+function blink(hbelm){
+  var ms = (tempoToMs()) + "ms";
+  hbelm.style.opacity = 0.5;
+  hbelm.style.animationDuration = ms;
+  setTimeout(function(){
+    hbelm.style.animationDuration = 0;
+    hbelm.style.opacity = 0;
+  }, tempoToMs());
+}
+
 var testArr = document.getElementsByClassName("button");
+var hbArr = document.getElementsByClassName("hiddenBox")
 var clock;
+var is_playing = false;
 
 function play(){
-  var i = 0;
-  var clock = setInterval(function loop(){
-    
-    //put this outside eventually
-    s = document.getElementById("stop");
-    s.addEventListener("click", function(){
-      clearInterval(clock);
-      console.log("uhde");
-    });
-    
+  if(is_playing == false){
+    is_playing = true;
+    var i = 0;
+    clock = setInterval(function loop(){
+      
     //bleh
     var eight = i+8;
     for(i; i<eight; i++){
       var style = window.getComputedStyle(testArr[i], null);
+      blink(hbArr[ (eight/8)-1 ]);
       if(style.backgroundColor == def_color){
-        console.log("NOTHING", i);
+        //console.log("NOTHING", i);
       }
       else{
         playAudio(testArr[i]);
-        console.log("PLAY SOUND", i, style.backgroundColor);
+        boom(testArr[i]);
+        //console.log("PLAY SOUND", i, style.backgroundColor);
       }
     }
     if(i > 63){
       i = 0;
       /*
+      for only one playthrough
       clearInterval(clock);
       */
     }
-  }, 500);
+  }, tempoToMs());
+  }else if(is_playing == true){
+    alert("It's already playing..");
+  }
+}
+
+function reset(){
+  //add code
+  var i=0;
+  for(i=0; i<64; i++){
+    testArr[i].style.backgroundColor = def_color;
+  }
 }
