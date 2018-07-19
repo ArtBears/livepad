b2f8b03597771a11d8a06a8305b7358f60c8525a
Glyne Gittens <jgit121@gmail.com>
2018-07-19 17:56:22 -0400

    Added routes sending audio files to front end and working on mongo schemas WIP

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
+const schemas = JSON.parse(fs.readFileSync('./schema.json', 'utf8')).schemas;
 global.appRoot = path.resolve(__dirname);
 
 app = express();
@@ -11,7 +12,10 @@ app = express();
 /* routes */
 const index = require('./routes/index.js');
 
-// Middleware
+/* collections  */
+const liveCollections = ["Users", "Sessions", "Songs"];
+
+/* Middleware */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
@@ -35,22 +39,43 @@ app.listen(3000, () => {console.log("app running")});
 // Comment out MongoClient block to run without DB client
 // start of MongoClient Block
 // Doesn't start app without successful connection to DB
-// MongoClient.connect("mongodb://localhost:27017/livepad", function(err, database){
-// 	if(err){
-// 		console.log(err);
-// 	}
-// 	else {
-// 		// coneect database instance to the request object
-// 		app.use( (req, res, next) => {
-// 			req.db = database;
-// 			console.log(req.db);
-// 			next()
-// 		})
-// 		express.request.db = database;
-// 		console.log("DB CONNECTED!");
-// 		app.listen(3000, () => { console.log(" App Listening on Port 3000 ") }) 
-// 	}
-// })
+MongoClient.connect("mongodb://localhost:27017", function(err, client){
+ 	if(err){
+ 		console.log(err);
+ 	}
+ 	else {
+        var db = client.db("livepad");
+ 		// coneect database instance to the request object
+ 		app.use( (req, res, next) => {
+ 			req.db = db;
+ 			console.log(req.db);
+ 			next();
+ 		})
+ 		express.request.db = db;
+ 		console.log("DB CONNECTED!");
+        db.listCollections().toArray( (err, items) => {
+            for (let col in liveCollections){
+                // check if liveCollections[col] is in items
+                if(!items.includes(liveCollections[col])){
+                    // no? create the collections
+                    db.createCollection(
+                        liveCollections[col], 
+                        schemas[liveCollections[col].toLowerCase()]        
+                    ).catch((err) => {
+                        console.log("Collection Create or Validation Error: " + err);
+                        exit();
+                    });
+                } else {
+                    // yes? continue;
+                    continue;
+                }
+            }  
+        })
+
+
+ 		app.listen(3000, () => { console.log(" App Listening on Port 3000 ") }); 
+ 	}
+ })
 
 // end of MongoClient Block
 module.exports = app;
