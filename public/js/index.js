947189e386a366af48ef38432f680a7465d2c808
kre64 <kre9404@gmail.com>
2018-07-24 16:57:40 -0400

    a

--- public/js/index.js
+++ public/js/index.js
@@ -86,7 +86,7 @@ mediaRecorder.onstop = function(e){
   
 };
 
-var kick = new Audio('public/audio/Samples1/hhat1.wv'); // fix this
+var kick = new Audio('js/samples/kick1.wv'); // fix this
 
 //clean this up eventually..
 //as of now each call to this function creates and plays
