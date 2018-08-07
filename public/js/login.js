75c81d066fb7761e56c6b39e8d4297553f78fb89
kre64 <kre9404@gmail.com>
2018-08-07 17:07:28 -0400

    basiclogic

--- public/js/login.js
+++ public/js/login.js
@@ -6,3 +6,25 @@ login page
     Create routes (POST & GET)
     Should query the DB for the form data, if null return error
 */
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:3000/";
+
+
+//get username from form
+var username = "testname";
+
+function isUser(username){
+	var inputname = username;
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+  		var dbo = db.db("livepad");
+  		var query = { user: inputname };
+  		dbo.collection("users").find(query).toArray(function(err, result) {
+  			if (err) throw err;
+  			console.log(result);
+  			//
+   			db.close();
+   		});
+  	});
+}
--- public/js/signup.js
+++ public/js/signup.js
@@ -9,3 +9,25 @@ signup page
         yes: say the user exists, ask if they want to login
     Redirect to session list with user
 */
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:3000/";
+
+
+//get username from form
+var username = "testname";
+
+function isUser(username){
+	var inputname = username;
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+  		var dbo = db.db("livepad");
+  		var query = { user: inputname };
+  		dbo.collection("users").find(query).toArray(function(err, result) {
+  			if (err) throw err;
+  			console.log(result);
+  			//
+   			db.close();
+   		});
+  	});
+}
