bb56fc8e6058145f64cf75a6057f17bd9f0010b0
Kevin Yan <lilkev9412@gmail.com>
2018-08-08 23:06:50 -0700

    added the streaming code to listen.js

--- public/js/listen.js
+++ public/js/listen.js
@@ -1,21 +1,67 @@
-/*route for song location
+(function(){
 
-  "/song/upload/test/:song_name"
+var codec = '';
 
-*/
+codec = 'audio/mpeg';
 
+//Locate the audio tag with id of THESTREAM
+var audio = document.getElementById('THESTREAM');
+var mediaSource = new MediaSource();
+audio.src = window.URL.createObjectURL(mediaSource);
+var buffer = null;
+var queue = [];
 
-//not important
-function initSong(){
-  
-  var song_name = "testname";
-  
-  var fd = new FormData();
-  fd.append('acorn', blob, song_name + ".ogg");
-  fetch("/song/upload/"+song_name, 
-  {
-    method: 'get',
-    body: fd
-  });
+var bufferArray = [];
+
+function updateBuffer(){
+    if (queue.length > 0 && !buffer.updating) {
+        buffer.appendBuffer(queue.shift());
+    }
+}
+
+//Mediasource
+
+function sourceBufferHandle(){
+    buffer = mediaSource.addSourceBuffer(codec);
+    buffer.mode = 'sequence';
+
+    buffer.addEventListener('update', function() {
+        console.log('update');
+        updateBuffer();
+    });
+
+    buffer.addEventListener('updateend', function() {
+        console.log('updateend');
+        updateBuffer();
+    });
+
+    initWS();
+}
+
+mediaSource.addEventListener('sourceopen', sourceBufferHandle)
+
+function initWS(){
+    var ws = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port, 'echo-protocol');
+    ws.binaryType = "arraybuffer";
+
+    ws.onopen = function(){
+        console.info('WebSocket connection initialized');
+    };
+
+    ws.onmessage = function (event) {
+        console.info('Recived WS message.', event);
+
+        if(typeof event.data === 'object'){
+            if (buffer.updating || queue.length > 0) {
+                queue.push(event.data);
+            } else {
+                buffer.appendBuffer(event.data);
+                audio.play();
+            }
+        }
+    };
 
 }
+
+
+})();
