4d574c98bb26e87f12120862d9d96aead6b69c6b
kre64 <kre9404@gmail.com>
2018-08-14 23:57:14 -0400

    Included Kevin Y's overtime submissions

--- README.txt
+++ README.txt
@@ -36,5 +36,31 @@ To run source code locally
 	node
 	npm
 
+-Ensure you have mongodb
+// mongodb setup - credit to Glyne G
+	check if you have a /data/db directory on your system
+	No? create it ' sudo mkdir /data  '  then ' sudo mkdir /data/db '
+	Yes? Gratz
+Change permissions for those directories
+	sudo chmod 777 /data
+	sudo chmod 777 /data/db
+Start the mongo process ' mongod & '
+	The '&' tells it to run in the background
+	press enter if the command prompt doesn't come back
+To use the mongo interactive shell run ' mongo  '
+	mongod must be running already for this to work
+	to check: use ' ps aux | grep mongo '
+	if you see more than one result, its running already
+	if this is your first time starting mongo, create the livepad database by typing ' use livepad '  
+
 -Run node index.js
 -Visit http://localhost:3000/
+
+
+site flow
+
+signup if you dont have an account, otherwise login with your account
+	@session/list/yourUSERid -> join a session in progress OR click on new at the top of the list
+		@session/sessionID/userID -> click create to start using the pad to make music OR click listen to listen to user made music
+			@session/createSong -> make music and then name your song, upload it
+				site was never finished :(
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var http = require('http');
+var net = require('net');
+var WebSocketServer = require('websocket').server;
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 const schemas = JSON.parse(fs.readFileSync('./schema.json', 'utf8')).schemas;
 global.appRoot = path.resolve(__dirname);
+
+var firstPacket = [];
+
+var options = {
+    tcpPort: 9090
+};
 
 app = express();
 app.locals.session_path = global.appRoot + "/sessions/";
@@ -39,6 +48,64 @@ app.use(express.static('public/audio/Samples1/hhat1.wv'));
 
 app.use('/', index);
 
+var wsClients = [];
+
+//HTTP Server
+var server = http.createServer(app);
+
+//TCP Server
+var tcpServer = net.createServer(function(socket) {
+    socket.on('data', function(data){
+
+      /**
+       * We are saving first packets of stream. These packets will be send to every new user.
+       * This is hack. Video won't start whitout them.  
+       */
+      if(firstPacket.length < 3){ 
+        console.log('Init first packet', firstPacket.length);
+        firstPacket.push(data); 
+      }
+
+      /**
+       * Send stream to all clients
+       */
+      wsClients.map(function(client, index){
+        client.sendBytes(data);
+      });
+    });
+});
+
+tcpServer.listen(options.tcpPort, 'localhost');
+
+/** Websocket */
+var wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+
+  if(firstPacket.length){
+    /**
+     * Every user will get beginning of stream 
+    **/ 
+    firstPacket.map(function(packet, index){
+      connection.sendBytes(packet); 
+    });
+    
+  }
+    
+  /**
+   * Add this user to collection
+   */
+  wsClients.push(connection);
+
+  connection.on('close', function(reasonCode, description) {
+      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+});
 
 // To run app without database access uncomment the next line 
 //app.listen(3000, () => {console.log("app running")});
@@ -80,7 +147,7 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client){
             }   
         })
         
- 		app.listen(3000, () => { console.log(" App Listening on Port 3000 ") }); 
+ 		server.listen(3000, () => { console.log(" App Listening on Port 3000 ") }); 
  	}
  })
 
